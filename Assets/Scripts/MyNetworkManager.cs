f42cca3b1e780af21399b15f005611c5a8465ed7
EvertonHimself <evertondeoliveira@gmail.com>
2016-01-15 13:58:56 -0200

    Testing the client/server connection.

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+/// <summary>
+/// My network HUD.
+/// </summary>
+public class MyNetworkManager : NetworkManager 
+{
+//	private NetworkManager networkManager;
+
+	void Start()
+	{
+//		networkManager = GetComponent<NetworkManager>();
+	}
+
+	/// <summary>
+	/// My start host method.
+	/// </summary>
+	void MyStartHost()
+	{
+//		networkManager.StartHost();
+		StartHost();
+		Debug.Log (Time.timeSinceLevelLoad + " Starting host.");
+	}
+
+	/// <summary>
+	/// Raises the start host event.
+	/// </summary>
+	public override void OnStartHost()
+	{
+		Debug.Log (Time.timeSinceLevelLoad + " Host started.");
+	}
+
+	/// <summary>
+	/// Raises the start client event.
+	/// </summary>
+	/// <param name="myClient">My client.</param>
+	public override void OnStartClient(NetworkClient myClient)
+	{
+		Debug.Log (Time.timeSinceLevelLoad + " Client start requested.");
+	}
+
+	/// <summary>
+	/// Raises the client connet event.
+	/// </summary>
+	/// <param name="conn">Conn.</param>
+	public override void OnClientConnect(NetworkConnection conn)
+	{
+		Debug.Log (Time.timeSinceLevelLoad + " Client is connected to IP: " + conn.address);
+	}
+}
