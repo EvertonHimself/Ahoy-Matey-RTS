8a32f14cf3c927086199c42df62e4a46ab0f95ef
EvertonHimself <evertondeoliveira@gmail.com>
2016-01-15 16:30:58 -0200

    Added the namespace UnityEngin.Networking to the Player.cs script to make the Player movement independent between local player and network players.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using UnityStandardAssets.CrossPlatformInput;
 using System.Collections;
 
-public class Player : MonoBehaviour 
+public class Player : NetworkBehaviour 
 {
 	public Vector3 inputValue;
 
 	void Update()
 	{
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+
 		inputValue.x = CrossPlatformInputManager.GetAxis ("Horizontal");
 		inputValue.y = 0f;
 		inputValue.z = CrossPlatformInputManager.GetAxis ("Vertical");
-	
+
 		transform.Translate (inputValue);
 	}
 }
