93fa2290fb9d4f4a4f4be7deac400d2cf9c4b4df
EvertonHimself <evertondeoliveira@gmail.com>
2016-01-15 13:06:19 -0200

    MyNetworkHUD.cs updated.

--- Assets/Scripts/MyNetworkHUD.cs
+++ Assets/Scripts/MyNetworkHUD.cs
@@ -5,29 +5,29 @@ using UnityEngine.Networking;
 /// <summary>
 /// My network HUD.
 /// </summary>
-public class MyNetworkHUD : MonoBehaviour 
+public class MyNetworkHUD : NetworkManager 
 {
-	private NetworkManager networkManager;
+//	private NetworkManager networkManager;
 
 	void Start()
 	{
-		networkManager = GetComponent<NetworkManager>();
+//		networkManager = GetComponent<NetworkManager>();
 	}
 
 	/// <summary>
 	/// My start host method.
 	/// </summary>
-	public void MyStartHost()
+	public override void StartHost()
 	{
-		networkManager.StartHost();
-		Debug.Log ("Starting host at " + Time.timeSinceLevelLoad);
+//		networkManager.StartHost();
+		Debug.Log ("Starting host at: " + Time.timeSinceLevelLoad);
 	}
 
 	/// <summary>
 	/// Raises the start host event.
 	/// </summary>
-	void OnStartHost()
+	public override void OnStartHost()
 	{
-		Debug.Log ("Host started at (OnStartHost)" + Time.timeSinceLevelLoad);
+		Debug.Log ("Host started at (OnStartHost): " + Time.timeSinceLevelLoad);
 	}
 }
