be21a2ee793a6ef79d625967ed72c82d6e0a66bc
EvertonHimself <evertondeoliveira@gmail.com>
2016-01-15 16:19:44 -0200

    Inported the CrossPlatformInput package; Added a Player.cs script to the Player GameObject.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+	public Vector3 inputValue;
+
+	void Update()
+	{
+		inputValue.x = CrossPlatformInputManager.GetAxis ("Horizontal");
+		inputValue.y = 0f;
+		inputValue.z = CrossPlatformInputManager.GetAxis ("Vertical");
+	
+		transform.Translate (inputValue);
+	}
+}
