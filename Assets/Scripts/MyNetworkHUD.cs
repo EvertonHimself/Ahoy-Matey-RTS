442e1e7ddd208a826d754973039d2d10aecf4113
EvertonHimself <evertondeoliveira@gmail.com>
2016-01-15 13:05:34 -0200

    Initial Environment Setup.

---
+++ Assets/Scripts/MyNetworkHUD.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+/// <summary>
+/// My network HUD.
+/// </summary>
+public class MyNetworkHUD : MonoBehaviour 
+{
+	private NetworkManager networkManager;
+
+	void Start()
+	{
+		networkManager = GetComponent<NetworkManager>();
+	}
+
+	/// <summary>
+	/// My start host method.
+	/// </summary>
+	public void MyStartHost()
+	{
+		networkManager.StartHost();
+		Debug.Log ("Starting host at " + Time.timeSinceLevelLoad);
+	}
+
+	/// <summary>
+	/// Raises the start host event.
+	/// </summary>
+	void OnStartHost()
+	{
+		Debug.Log ("Host started at (OnStartHost)" + Time.timeSinceLevelLoad);
+	}
+}
